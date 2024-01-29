cca612b7476517d0745b7c841bde176fe0999690
Dillon van Dorssen <122318528+VondleCode@users.noreply.github.com>
2024-01-30 00:49:45 +1100

    add prompt for user and add function to play one round.

--- rps.js
+++ rps.js
@@ -1,10 +1,27 @@
+let player = prompt("It's time! Rock, Paper or Scissors?");
+
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
-    
+    const choices = ["rock", "paper", "scissors"];
     const random = Math.floor(Math.random() * choices.length);
-    console.log(choices[random]);
+    return choices[random];
 }
 
-function getPlayerChoice {
-    
+
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === computerSelection) {
+        return "It's a Draw!"
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You Win! Rock beats Scissors"
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You Win! Paper beats Rock"
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win! Scissors beats Paper"
+    } else {
+        return "You Lose! Computer chose " + computerSelection
+    }
 }
+
+const playerSelection = player;
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
