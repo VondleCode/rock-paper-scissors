1d385dd2b51d659098d8bd36cdb5e043461f437c
Dillon van Dorssen <122318528+VondleCode@users.noreply.github.com>
2024-01-29 22:36:07 +1100

    Add computer rock paper scissor choice function

---
+++ rps.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    
+    const random = Math.floor(Math.random() * choices.length);
+    console.log(choices[random]);
+}
+
+function getPlayerChoice {
+    
+}
